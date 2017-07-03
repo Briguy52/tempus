d813949e6359cd7e300abe9b6fa2fc662a1d44f6
Briguy52 <Briguy52@gmail.com>
2017-07-02 20:14:13 -0400

    added basic dashboard component; routing works

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './Dashboard.scss'
+
+class Dashboard extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <div className={classes.background}>
+          <h2>Tempus</h2>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
